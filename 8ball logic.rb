a8622f9463d3bf052d407468a2420d6550ab6adc
Houston <Houston@evilgeniustechnologies.com>
2016-09-02 20:21:59 -0400

    Created 8 ball logic file, which will be the only file (and have the script) for the Ruby version of my 8 ball until I am familiar with rails and add controllers, javascript, etc. etc.

---
+++ 8ball logic.rb
@@ -0,0 +1,6 @@
+=begin
+This is the primary code for the Magic 8 Ball.
+Since I'm still waiting on learning Rails, I am just doing it as
+a single .RB file to run as a sript until I have web integration.
+=end
+
