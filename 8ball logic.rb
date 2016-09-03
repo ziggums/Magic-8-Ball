66a6af2220741856ed79c5cca18002809cffaffa
Houston <Houston@evilgeniustechnologies.com>
2016-09-02 22:04:35 -0400

    began verbiage

--- 8ball logic.rb
+++ 8ball logic.rb
@@ -4,3 +4,14 @@ Since I'm still waiting on learning Rails, I am just doing it as
 a single .RB file to run as a sript until I have web integration.
 =end
 
+puts "Hi! Welcome to the Magic 8 Ball!"
+puts "Whats your name?"
+username = gets.chomp
+puts "Hi, #{username}!"
+puts "Which version of the Magic 8 Ball would you like?"
+puts "\n 1. Original"
+puts "\n 2. Yesman 3000"
+puts "\n 3. ...in bed"
+puts "\n 4. Explicit"
+puts "Type the number to select: "
+answer_type = gets.chomp
